d6d43b2e857339c7f0c9542032046f5c8813ff58
emjayoh <emjayoh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-27 18:30:57 +0000

    target-page-navigation - updated license to GPL
    git-svn-id: https://plugins.svn.wordpress.org/target-page-navigation/trunk@89810 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- target-page-navigation.php
+++ target-page-navigation.php
@@ -8,13 +8,24 @@ Version: 0.1.1
 Author URI: http://www.siteRighter.com
 */ 
 
-
 /*
 
-This plugin is owned by Onewebsite and is currently unlicensed. Please defer any questions to michael@onewebsite.ca
+Copyright 2007 Mike Olaski (mjo@emjayoh.com)
 
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
-
 
 function &get_navtype_pages($args = '') {
 	global $wpdb;
